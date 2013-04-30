85cf09db45e38f0698cd33353fdd6fa344318a8c
Tomasz Drzewiecki <kn65op@gmail.com>
2013-04-30 18:12:59 +0200

    Add OpenCL3DImageAlgorithm class

---
+++ OpenCLInterface/OpenCl3DImageAlgorithm.cpp
@@ -0,0 +1,11 @@
+#include "OpenCl3DImageAlgorithm.h"
+
+
+OpenCl3DImageAlgorithm::OpenCl3DImageAlgorithm(void)
+{
+}
+
+
+OpenCl3DImageAlgorithm::~OpenCl3DImageAlgorithm(void)
+{
+}
---
+++ OpenCLInterface/OpenCl3DImageAlgorithm.h
@@ -0,0 +1,8 @@
+#pragma once
+class OpenCl3DImageAlgorithm
+{
+public:
+  OpenCl3DImageAlgorithm(void);
+  ~OpenCl3DImageAlgorithm(void);
+};
+
