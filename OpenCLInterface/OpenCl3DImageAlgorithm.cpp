22d24deb72847d0f77691347d5be0e7b9cd10a01
Tomasz Drzewiecki <kn65op@gmail.com>
2013-05-01 10:59:35 +0200

    some coding on 3D images

--- OpenCLInterface/OpenCl3DImageAlgorithm.cpp
+++ OpenCLInterface/OpenCl3DImageAlgorithm.cpp
@@ -1,11 +1,80 @@
 #include "OpenCl3DImageAlgorithm.h"
 
-
-OpenCl3DImageAlgorithm::OpenCl3DImageAlgorithm(void)
-{
+#define ASSERT_OPENCL_ERR(ERR,MSG) if(ERR != CL_SUCCESS) \
+{ \
+  throw OpenCL3DImageException(MSG, ERR); \
 }
 
-
-OpenCl3DImageAlgorithm::~OpenCl3DImageAlgorithm(void)
+OpenCL3DImageAlgorithm::OpenCL3DImageAlgorithm(void)
 {
+  input_image_memory = output_image_memory = nullptr;
+  width = height = depth = 0;
+  prepared = false;
+}
+
+OpenCL3DImageAlgorithm::~OpenCL3DImageAlgorithm(void)
+{
+  clearAlgorithm();
+}
+
+void OpenCL3DImageAlgorithm::clearAlgorithm()
+{
+  if (input_image_memory != nullptr)
+  {
+    clReleaseMemObject(input_image_memory);
+  }
+  if (output_image_memory != nullptr)
+  {
+    clReleaseMemObject(output_image_memory);
+  }
+}
+
+void OpenCL3DImageAlgorithm::setDataSize(size_t w, size_t h, size_t d)
+{
+  width = w;
+  height = h;
+  depth = d;
+}
+
+void OpenCL3DImageAlgorithm::prepare()
+{
+  if (prepared)
+  {
+    //clear old algo
+  }
+
+  cl_int err;
+
+  input_image_memory = clCreateImage3D(context, CL_MEM_READ_ONLY, &input_image_format, width, height, depth, 0, 0, NULL, &err);
+  ASSERT_OPENCL_ERR(err, "Error while creating image3D for input in OpenCL3DImageAlgorithm");
+
+  err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *) &input_image_memory);
+  ASSERT_OPENCL_ERR(err, "Cant set kernel arg 0 of OpenCL3DImageAlgorithm");
+
+  output_image_memory = clCreateImage3D(context, CL_MEM_READ_ONLY, &output_image_format, width, height, depth, 0, 0, NULL, &err);
+  ASSERT_OPENCL_ERR(err, "Error while creating image3D for output in OpenCL3DImageAlgorithm");
+
+  err = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *) &output_image_memory);
+  ASSERT_OPENCL_ERR(err, "Cant set kernel arg 1 of OpenCL3DImageAlgorithm");
+
+  prepared = true;
+}
+
+void OpenCL3DImageAlgorithm::processImage(const void* data_input, void* data_output)
+{
+  if (!prepared)
+  {
+    throw OpenCL3DImageException("Algorithm is not preapred");
+  }
+
+  cl_int err;
+  time = 0;
+
+  size_t origin[] = {0,0,0};
+  size_t region[] = {width, height, depth};
+
+  err = clEnqueueWriteImage(command_queue, input_image_memory, CL_FALSE, origin, region, 0, 0, data_input, 0, NULL, NULL);
+  ASSERT_OPENCL_ERR(err, "Error while enqueue write image 3D");
+
+
 }
--- OpenCLInterface/OpenCl3DImageAlgorithm.h
+++ OpenCLInterface/OpenCl3DImageAlgorithm.h
@@ -8,25 +8,29 @@
  */
 class OpenCL3DImageException : public OpenCLException
 {
-  OpenCL3DImageException(std::string m, int e = 0) : OpenCLException(m,e) {}
+public:
+
+  OpenCL3DImageException(std::string m, int e = 0) : OpenCLException(m, e)
+  {
+  }
 };
 
 /** Class for algorithms manipulating 3D images.
  * Simple framework.
  */
-class OpenCl3DImageAlgorithm
+class OpenCL3DImageAlgorithm
 {
 public:
   /**
    * Default contructor.
    */
-  OpenCl3DImageAlgorithm(void);
-  
+  OpenCL3DImageAlgorithm(void);
+
   /**
    * Destructor.
    */
-  ~OpenCl3DImageAlgorithm(void);
-  
+  ~OpenCL3DImageAlgorithm(void);
+
   /**
    * Set 3D image size.
    * @param w Image width.
@@ -34,19 +38,19 @@ public:
    * @param d Image depth.
    */
   void setDataSize(size_t w, size_t h, size_t d);
-  
+
   /**
    * Prepare algorithm.
    */
   void prepare();
-  
+
   /**
    * Process 3D image.
    * @param data_input Pointer to continious memory with input 3D image. 
    * @param data_output  Pointer to continious memory to store output 3D image. 
    */
   void processImage(const void * data_input, void * data_output);
-  
+
   /**
    * Get gime consumed on algorithm execution.
    * @return Time in ms.
@@ -56,17 +60,26 @@ protected:
 
 
 private:
+  size_t height, width, depth;
+
   OpenCLDevice device;
+  cl_kernel kernel;
+
   double time;
-  
+
   cl_context context;
   cl_command_queue command_queue;
-  
+
   cl_mem input_image_memory;
   cl_mem output_image_memory;
-  
+
   cl_image_format input_image_format;
   cl_image_format output_image_format;
-  
+
   bool prepared;
+
+  /**
+   * Clears algorithm.
+   */
+  void clearAlgorithm();
 };
