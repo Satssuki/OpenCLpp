ca78bef8bf902a160502ea9c5be046d312fd6205
Tomasz Drzewiecki <kn65op@gmail.com>
2013-05-01 11:28:32 +0200

    Add getTime to OpenCLCommon

--- OpenCLInterface/OpenCLCommon.h
+++ OpenCLInterface/OpenCLCommon.h
@@ -33,8 +33,24 @@ public:
   void createKernel();
   
   /**
+   * Run kernel.
+   * @param work_dim Dimensions of data processed.
+   * @param global_work_offset Work offset (where to start in regarding input pointer).
+   * @param global_work_size Work size (can be image size).
+   * @param local_work_size Local work size (how big data one computing unit is processing in time).
+   * @param num_events_in_wait_list Put 0.
    */
   void enqueueNDRangeKernelWithTimeMeasurment(cl_uint work_dim, size_t * global_work_offset, const size_t *global_work_size, const size_t *local_work_size, cl_uint num_events_in_wait_list);
+
+  /**
+   * Get time consumed by kernel.
+   * @return Time in ms in double.
+   */  
+  double getTime() const
+  {
+    return time;
+  }
+
   
 protected:
   /** file name with kernel source
