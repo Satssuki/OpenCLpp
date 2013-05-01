d4a48ebe6e8d02efde287c9bda178cc2a863dfd3
Tomasz Drzewiecki <kn65op@gmail.com>
2013-05-01 11:57:28 +0200

    Add files

---
+++ OpenCLInterface/OpenCLFindMaxin3DImage.cpp
@@ -0,0 +1,19 @@
+#include "OpenCLFindMaxin3DImage.h"
+
+
+OpenCLFindMaxin3DImage::OpenCLFindMaxin3DImage(void)
+{
+  source_filename = "max.cl";
+  kernel_name = "findLocalMax";
+
+  input_image_format.image_channel_data_type = CL_UNSIGNED_INT8;
+  input_image_format.image_channel_order = CL_LUMINANCE;
+    
+  input_image_format.image_channel_data_type = CL_UNSIGNED_INT8;
+  input_image_format.image_channel_order = CL_LUMINANCE;
+}
+
+
+OpenCLFindMaxin3DImage::~OpenCLFindMaxin3DImage(void)
+{
+}
---
+++ OpenCLInterface/OpenCLFindMaxin3DImage.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "OpenCl3DImageAlgorithm.h"
+
+class OpenCLFindMaxin3DImage :  public OpenCL3DImageAlgorithm
+{
+public:
+  OpenCLFindMaxin3DImage(void);
+  ~OpenCLFindMaxin3DImage(void);
+};
+
