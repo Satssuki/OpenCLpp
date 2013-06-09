03323ee932ecd2677e4d75e2f265dab60a75c5db
Tomasz Drzewiecki <kn65op@gmail.com>
2013-06-09 15:27:31 +0200

    Add 2DTo2D algoritm

---
+++ OpenCLInterface/OpenCL2DTo2DImageAlgorithm.cpp
@@ -0,0 +1,46 @@
+#include "OpenCL2DTo2DImageAlgorithm.h"
+
+
+#define ASSERT_OPENCL_ERR(ERR,MSG) if(ERR != CL_SUCCESS) \
+{ \
+  throw OpenCL2DTo2DImageException      (MSG, ERR); \
+}
+
+
+OpenCL2DTo2DImageAlgorithm::OpenCL2DTo2DImageAlgorithm(void) : OpenCLImageCommon()
+{
+  width = height = depth = 0;
+}
+
+
+OpenCL2DTo2DImageAlgorithm::~OpenCL2DTo2DImageAlgorithm(void)
+{
+}
+
+void OpenCL2DTo2DImageAlgorithm::setDataSize(size_t w, size_t h)
+{
+  width = w;
+  height = h;
+}
+
+void OpenCL2DTo2DImageAlgorithm::setIOArguments()
+{
+  cl_int err;
+
+  input_image_memory = clCreateImage2D(context, CL_MEM_READ_ONLY, &input_image_format, width, height, 0, NULL, &err);
+  ASSERT_OPENCL_ERR(err, "Error while creating image2D for input in OpenCL2DTo2DImageAlgorithm");
+
+  err = setKernelArg(0, &input_image_memory);
+  ASSERT_OPENCL_ERR(err, "Cant set kernel arg 0 of OpenCL2DTo2DImageAlgorithm");
+
+  output_image_memory = clCreateImage2D(context, CL_MEM_READ_WRITE, &output_image_format, width, height, 0 , NULL, &err);
+  ASSERT_OPENCL_ERR(err, "Error while creating image2D for output in OpenCL2DTo2DImageAlgorithm");
+
+  err = setKernelArg(1, &output_image_memory);
+  ASSERT_OPENCL_ERR(err, "Cant set kernel arg 1 of OpenCL2DTo2DImageAlgorithm");
+}
+
+void OpenCL2DTo2DImageAlgorithm::setOtherArguments()
+{
+  //none
+}
---
+++ OpenCLInterface/OpenCL2DTo2DImageAlgorithm.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "OpenCLImageCommon.h"
+
+/** Exception for OpenCL3DTo3DImageAlgorithm
+ * 
+ */
+class OpenCL2DTo2DImageException : public OpenCLException
+{
+public:
+
+  OpenCL2DTo2DImageException(std::string m, int e = 0) : OpenCLException(m, e)
+  {
+  }
+};
+
+class OpenCL2DTo2DImageAlgorithm : public OpenCLImageCommon
+{
+public:
+  /** 
+   * Default constructor.
+   */
+  OpenCL2DTo2DImageAlgorithm(void);
+    
+  /**
+   * Destructor.
+   */
+  ~OpenCL2DTo2DImageAlgorithm(void);
+
+   /**
+   * Set 2D and 2D image size. For 2D images only width and height is relevatnt.
+   * @param w Image width.
+   * @param h Image height.
+   */
+  void setDataSize(size_t w, size_t h);
+
+  protected:
+  /**
+   * set IO args.
+   */
+  void setIOArguments();
+  
+  /**
+   * set other args.
+   */
+  void setOtherArguments();
+};
